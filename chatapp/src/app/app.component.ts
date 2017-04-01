88a90fb3575c93c6a4ed1024ae7c8481039d7cdb
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-04-01 11:36:35 +0300

    update style with chat view page

--- chatapp/src/app/app.component.ts
+++ chatapp/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component,Inject } from '@angular/core';
+import { Platform,NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import {LoginPage}from '../pages/login/login';
 
+import {AuthProvider}from '../providers/auth';
+import {ChatProvider}from '../providers/chat';
+import {UserProvider}from '../providers/user';
+import {UtilProvider}from '../providers/util';
 
 import {
 FIREBASE_PROVIDERS,
@@ -24,11 +29,22 @@ AuthMethods
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any ;
   message:string;
 
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,public authProvider:AuthProvider,
+
+  ) {
+    let auth = authProvider.getAuth();
+    auth.onAuthStateChanged(user => {
+      if(user) {
+        this.rootPage =TabsPage;
+      } else {
+        this.rootPage = LoginPage;
+      }
+    })
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
